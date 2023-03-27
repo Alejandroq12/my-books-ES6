2e623bf089a530fb9b98c37a96adc5338bd65523
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-03-27 13:00:03 -0600

    Add export default to navigation.js module

--- modules/navigation.js
+++ modules/navigation.js
@@ -1,29 +1,30 @@
-export const setupNavigation = () => {
-    const navBooks = document.getElementById('navBooks');
-    const navAddBook = document.getElementById('navAddBook');
-    const navContact = document.getElementById('navContact');
-  
-    const booksSection = document.getElementById('booksSection');
-    const addBookSection = document.getElementById('addBookSection');
-    const contactSection = document.getElementById('contactSection');
-  
-    const showSection = (section) => {
-      booksSection.classList.add('hidden');
-      addBookSection.classList.add('hidden');
-      contactSection.classList.add('hidden');
-      section.classList.remove('hidden');
-    };
-  
-    navBooks.addEventListener('click', () => {
-      showSection(booksSection);
-    });
-  
-    navAddBook.addEventListener('click', () => {
-      showSection(addBookSection);
-    });
-  
-    navContact.addEventListener('click', () => {
-      showSection(contactSection);
-    });
+const setupNavigation = () => {
+  const navBooks = document.getElementById('navBooks');
+  const navAddBook = document.getElementById('navAddBook');
+  const navContact = document.getElementById('navContact');
+
+  const booksSection = document.getElementById('booksSection');
+  const addBookSection = document.getElementById('addBookSection');
+  const contactSection = document.getElementById('contactSection');
+
+  const showSection = (section) => {
+    booksSection.classList.add('hidden');
+    addBookSection.classList.add('hidden');
+    contactSection.classList.add('hidden');
+    section.classList.remove('hidden');
   };
-  
+
+  navBooks.addEventListener('click', () => {
+    showSection(booksSection);
+  });
+
+  navAddBook.addEventListener('click', () => {
+    showSection(addBookSection);
+  });
+
+  navContact.addEventListener('click', () => {
+    showSection(contactSection);
+  });
+};
+
+export default setupNavigation;
