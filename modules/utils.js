cd0b72aac5e60e89570f7ac1bd1d81953a45306b
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-03-27 13:11:45 -0600

    Fix Luxon error

--- modules/utils.js
+++ modules/utils.js
@@ -1,4 +1,4 @@
-import { DateTime } from '../node_modules/luxon/src/luxon.js';
+import { DateTime } from 'luxon';
 
 export const isValidInput = (input) => {
   if (input.trim() === '') return false;
