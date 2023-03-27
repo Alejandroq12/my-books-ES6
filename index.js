39516afaa9f9e3748d75f434254e25d18d04d602
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-03-27 12:44:10 -0600

    Add BookListUI module

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 // eslint-disable-next-line max-classes-per-file
 import BookList from './modules/bookList.js';
+import BooksListUI from './modules/booksListUI.js';
+
+
+
 
 
 document.addEventListener('DOMContentLoaded', () => {
   const bookList = new BookList();
   const booksListUI = new BooksListUI(bookList);
-
+  setupFormHandler(bookList, booksListUI);
+  booksListUI.render();
 });
